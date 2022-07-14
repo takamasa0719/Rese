26fc545fdddd2c6999aaa0eb453eada5bf8f23ba
takamasa0719 <theguitar74@gmail.com>
2022-07-14 09:38:37 +0900

    heroku5

---
+++ app/Models/shop.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'area_id',
+        'category_id',
+        'owner_id',
+        'name',
+        'overview',
+        'image_path',
+    ];
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasOne(Courses::class);
+    }
+}
